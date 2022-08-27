51bcd58aba82ae66671ea2199ed233e453a1867e
@rharison <rharison.abreu@gmail.com>
2022-08-27 09:04:59 -0300

    fix: adjustings

--- rate-place/src/App.jsx
+++ rate-place/src/App.jsx
@@ -3,13 +3,30 @@ import Button from '@mui/material/Button';
 import { getPlace } from '../public/uitl-axios.js'
 import { useEffect, useState } from 'react'
 
+
+
 function App() {
   const [place, setPlace] = useState(null)
+
+  async function setEvaluatedInPlace(place, evaluated) {
+    console.log('EVALUETED: ', evaluated)
+  }
 
   useEffect(() => {
     async function getNewPlace() {
       const place = await getPlace()
       setPlace(place)
+    }
+    document.onkeydown = (e) => {
+      const events = {
+        'ArrowRight': () => {
+          setEvaluatedInPlace(place, true)
+        },
+        'ArrowLeft': () => {
+          setEvaluatedInPlace(place, false)
+        },
+      }
+      events[e.key] && events[e.key]()
     }
     getNewPlace()
   }, [])
@@ -17,7 +34,7 @@ function App() {
   return (
     <div className="App">
       <section className='conteudo'>
-        <div className='containerTexto'>
+        <div className='container containerTexto'>
           <span><b>Nome:</b> {place?.mainName}</span>
           <span><b>Categoria:</b> {place?.category}</span>
           <span><b>Nota: </b> {place?.ratingInformation?.basicRatingInformation?.averageGrade}</span>
@@ -25,7 +42,7 @@ function App() {
           <span><b>Tem comentários?:</b> {place?.topComments.length ? 'Sim' : 'Não' }</span>
         </div>
 
-        <div className='containerFotos'>
+        <div className='container containerFotos'>
           {place?.featuredPhotos && place?.featuredPhotos.slice(0, 16).map(photo => {
             return <img className='foto' src={photo} alt='foto'/>
           })}
@@ -38,7 +55,7 @@ function App() {
           color="error"
           variant="contained"
           style={{fontSize: '3rem'}}
-          onClick={() => window.alert('Thander Bixão')}>RUIM
+          onClick={() => setEvaluatedInPlace(place, false)}>RUIM
         </Button>
 
         <Button
@@ -46,7 +63,7 @@ function App() {
           color="success"
           variant="contained"
           style={{fontSize: '3rem'}}
-          onClick={() => window.alert('Thander Bixão')}>BOM
+          onClick={() => setEvaluatedInPlace(place, true)}>BOM
         </Button>
       </div>
     </div>
